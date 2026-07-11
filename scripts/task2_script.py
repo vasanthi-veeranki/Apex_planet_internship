727f3c3e33965873929dddfcd76d539e1a57ebaa
vasanthi-veeranki <vasanthiveeranki17@gmail.com>
2026-07-11 22:29:38 +0530

    Add files via upload

---
+++ scripts/task2_script.py
@@ -0,0 +1,30 @@
+print("="*60)
+print("APEX PLANET - DATA SCIENCE INTERNSHIP")
+print("TASK 2 : SQL FOR DATA EXTRACTION")
+print("="*60)
+
+print("\nStep 1: Import the required libraries.")
+print("Imported Pandas and SQLAlchemy.")
+
+print("\nStep 2: Connect to the SQLite database.")
+print("Connection established successfully.")
+
+print("\nStep 3: Load the Superstore dataset.")
+print("Dataset imported into the SQLite database.")
+
+print("\nStep 4: Execute SQL queries.")
+print("-> Display first 5 records using SELECT.")
+print("-> Count total records using COUNT().")
+print("-> Filter records using WHERE.")
+print("-> Calculate total sales using SUM().")
+print("-> Calculate average profit using AVG().")
+print("-> Group data using GROUP BY.")
+print("-> Sort records using ORDER BY.")
+
+print("\nStep 5: Display query results.")
+print("The extracted data is displayed successfully.")
+
+print("\nStep 6: Conclusion")
+print("Task 2 completed successfully.")
+print("Python was integrated with SQLite using SQLAlchemy and Pandas.")
+print("Thank you!")
